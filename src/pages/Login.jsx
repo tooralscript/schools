ee7350c0e5fd0b83a3e899ef28c499b27dc8cb66
tooralscript <thasanov15638@ada.edu.az>
2024-05-28 15:08:11 +0400

    login box

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,12 +13,12 @@ export default function Login() {
 
           <div>
             <label htmlFor="username">Username</label>
-            <input type="text" id='username' name='username' />
+            <input type="text" id='username' name='username' required />
           </div>
 
           <div>
             <label htmlFor="password">Password</label>
-            <input type="text" id='password' name='password' />
+            <input type="text" id='password' name='password' required />
           </div>
 
           <button type='submit'>Login</button>
